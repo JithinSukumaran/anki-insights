9229b315a5a6ca7cf967dd35c3a85bd7cf73b596
jithin <jithin.sukumaran09@gmail.com>
2024-07-16 18:37:47 +0530

    adding new features such as total time & card count

--- __init__.py
+++ __init__.py
@@ -1,36 +1,108 @@
-# import the main window object (mw) from aqt
 from aqt import mw
-# import the "show info" tool from utils.py
-from aqt.utils import showInfo, qconnect
-# import all of the Qt GUI library
-from aqt.qt import *
+from aqt.qt import QAction, QDialog, QVBoxLayout, QLabel, QPushButton, QDialogButtonBox, QTextEdit
+from aqt.utils import qconnect, showInfo
+import time
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
+class StatisticsDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Anki Statistics")
+        self.setMinimumWidth(600)
+        self.setMinimumHeight(400)
+        
+        layout = QVBoxLayout()
 
-def testFunction() -> None:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    # show a message box
-    showInfo(f"total Card count: {cardCount}")
+        # Add a label for total number of cards
+        self.total_cards_label = QLabel()
+        layout.addWidget(self.total_cards_label)
 
-def testFunction2() -> None:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    
-    # show a message box
-    showInfo(f"Total Card count: {cardCount}")
+        # Add a label for total time elapsed in studies
+        self.total_time_label = QLabel()
+        layout.addWidget(self.total_time_label)
 
-# create a new menu item, "test"
-action = QAction("Test", mw)
-action2 = QAction('Test all', mw)
-# set it to call testFunction when it's clicked
-qconnect(action.triggered, testFunction)
-qconnect(action2.triggered, testFunction2)
-# and add it to the tools menu
+        # Add a text edit for cards per deck (this will include the bar chart)
+        self.cards_per_deck_chart = QTextEdit()
+        self.cards_per_deck_chart.setReadOnly(True)
+        layout.addWidget(self.cards_per_deck_chart)
+
+        # Add OK button
+        buttons = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
+        buttons.accepted.connect(self.accept)
+        layout.addWidget(buttons)
+
+        self.setLayout(layout)
+
+    def update_statistics(self, total_cards, total_time, cards_per_deck):
+        self.total_cards_label.setText(f"Total number of cards: {total_cards}")
+
+        # minutes, seconds = divmod(int(total_time), 60)
+        # self.total_time_label.setText(f"Total time elapsed in studies: {minutes} minutes and {seconds} seconds")
+
+
+        total_seconds = int(total_time)
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        self.total_time_label.setText(f"Total time elapsed in studies: {hours} hours : {minutes} minutes : {seconds} seconds")
+
+
+
+        # Generate the HTML content for the bar chart
+        html_content = self.generate_bar_chart_html(cards_per_deck)
+        self.cards_per_deck_chart.setHtml(html_content)
+
+    def generate_bar_chart_html(self, cards_per_deck):
+        # Create HTML content for the bar chart
+        bars = ""
+        for deck, count in cards_per_deck.items():
+            bar_width = count * 5  # Scale the bar width
+            bars += f"""
+            <div style="margin: 5px 0;">
+                <span style="display: inline-block; width: 150px;">{deck}</span>
+                <div style="display: inline-block; background-color: brown; width: {bar_width}px; height: 20px;"></div>
+                <span> {count} cards</span>
+            </div>
+            """
+        html = f"""
+        <html>
+        <head>
+            <style>
+                .bar-chart {{
+                    font-family: Arial, sans-serif;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="bar-chart">
+                {bars}
+            </div>
+        </body>
+        </html>
+        """
+        return html
+
+def show_statistics():
+    col = mw.col
+    if not col:
+        showInfo("No collection loaded.")
+        return
+
+    # Total number of cards
+    total_cards = col.card_count()
+
+    # Cards per deck
+    cards_per_deck = {deck['name']: col.db.scalar("SELECT COUNT() FROM cards WHERE did = ?", deck['id']) for deck in col.decks.all()}
+
+    # Total time elapsed in studies (in seconds)
+    total_time = col.db.scalar("SELECT SUM(time)/1000 FROM revlog")
+    if total_time is None:
+        total_time = 0
+
+    # Show the statistics dialog
+    dialog = StatisticsDialog(mw)
+    dialog.update_statistics(total_cards, total_time, cards_per_deck)
+    dialog.exec()
+
+# Create a new menu item in the Tools menu
+action = QAction("Show Anki Insights", mw)
+qconnect(action.triggered, show_statistics)
 mw.form.menuTools.addAction(action)
-mw.form.menuTools.addAction(action2)
-
-
